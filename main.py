8850d1456ddd7a3373dfc2e384a178b9867e2659
Hemory Phifer <hphife@yahoo.com>
2021-11-05 23:26:32 -0400

    Complete solution

---
+++ main.py
@@ -0,0 +1,7 @@
+# Exercise One
+birth_month = input("Enter your birth month: ")
+print(birth_month)
+
+# Challenge
+user_name = input("Enter name: ")
+print(f"{user_name}, your birth month is {birth_month}.")
